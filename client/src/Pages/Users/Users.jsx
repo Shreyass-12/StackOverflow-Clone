f4015debf28aad27a2d7e490af524f0eaba0b19f
Shreya <shreyaashreyaasingh00@gmail.com>
2023-02-08 12:56:51 +0530

    second commit

---
+++ client/src/Pages/Users/Users.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Users = () => {
+  return (
+    <div>
+      <h1>users page</h1>
+      <h1>users page</h1>
+      <h1>users page</h1>
+      <h1>users page</h1>
+      <h1>users page</h1>
+      <h1>users page</h1>
+    </div>
+  )
+}
+
+export default Users
