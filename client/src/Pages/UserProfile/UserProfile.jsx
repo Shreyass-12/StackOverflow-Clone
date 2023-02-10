45a474f01196559593c7adc687a91d5489e3e9ec
Shreya <shreyaashreyaasingh00@gmail.com>
2023-02-10 22:46:13 +0530

    commit

--- client/src/Pages/UserProfile/UserProfile.jsx
+++ client/src/Pages/UserProfile/UserProfile.jsx
@@ -1,21 +1,61 @@
-import React from 'react'
+import React,{ useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faBirthdayCake,faPen} from '@fortawesome/free-solid-svg-icons'
+import moment from 'moment'
 
 import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
 import Avatar from '../../components/Avatar/Avatar'
+import EditProfileForm from './EditProfileForm'
+import ProfileBio from './ProfileBio'
+import './UserProfile.css'
 
 const UserProfile = () => {
+
+    const { id } = useParams()
+    const users = useSelector((state) => state.usersReducer)
+    const currentProfile = users.filter((user) => user._id === id)[0] //profile when i click some user from user page
+    //console.log(currentProfile)
+    const currentUser = useSelector((state) => state.currentUserReducer)   //Profile where we are logged in
+    //console.log(currentProfile)
+    //console.log(currentUser)
+
+   const [Switch,setSwitch] = useState(false)
+
+
     return (
         <div className='home-container-1'>
             <LeftSidebar />
             <div className='home-container-2'>
                 <section>
                     <div className="user-details-container">
-                      <div className='user-details'>
-                        <Avatar >
-
-                        </Avatar>
-                      </div>
+                        <div className='user-details'>
+                            <Avatar backgroundColor="purple" color="white" fontSize="45px" px="40px" py="20px">
+                            {currentProfile?.name.charAt(0).toUpperCase()}
+                            </Avatar>
+                            <div className="user-name">
+                                <h1>{currentProfile?.name}</h1>
+                                <p><FontAwesomeIcon icon={faBirthdayCake} /> Joined {moment(currentProfile?.joinedOn).fromNow()}</p>
+                            </div>
+                        </div>
+                        {
+                            currentUser?.result._id === id && (
+                                <button type='button' onClick={ () => setSwitch(true) } className='edit-profile-btn'>
+                                <FontAwesomeIcon icon={faPen} /> Edit Profile
+                                </button>
+                            ) 
+                        }
                     </div>
+                    <>
+                    {
+                        Switch ? (
+                            <EditProfileForm currentUser={currentUser} setSwitch={setSwitch} />      //true
+                        ) : (
+                            <ProfileBio currentProfile={currentProfile} />           //false
+                        )
+                    }
+                    </>
                 </section>
             </div>
         </div>
--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -2,9 +2,9 @@ import jwt from 'jsonwebtoken'
 
 const auth = (req,res,next) => {
     try {
-        const token = req.data.Authorization.split(' ')[1]   // first string bracket represents bearer and 1 represents token
+        const token = req.headers.authorization.split(' ')[1]   // first string bracket represents bearer and 1 represents token
         
-        let decodeData = jwt.verify(token, 'test')
+        let decodeData = jwt.verify(token, process.env.JWT_SECRET)
         req.userId = decodeData?.id
         
         next()     //next is AskQuestion in routes Questions.js that will execute
